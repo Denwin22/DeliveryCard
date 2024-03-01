33c9b443d1bb9c5002a8d8f7385db58285b67283
Denwin <biscooter@mail.ru>
2024-03-02 00:22:52 +0600

    UpToDate

--- src/test/java/ru/netology/service/DeliveryCardTest.java
+++ src/test/java/ru/netology/service/DeliveryCardTest.java
@@ -29,4 +29,23 @@ public class DeliveryCardTest {
         $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + bookedDate));
     }
+
+
+    @Test
+    public void shouldValidDomainNameKg() {
+        open("https://www.cctld.kg/ru");
+        $(".nav-link.custom-nav-link[href='https://www.cctld.kg/ru/whois']").click();
+        $(".form-control.col-md-8").click();
+        $(".form-control.col-md-8").setValue("netology.kg");
+        $(".btn.btn-brand-01.col-md-4").click();
+        $(".col-md-6 p b").shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Доменное имя NETOLOGY.KG свободно для регистрации."));
+
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+    }
 }
